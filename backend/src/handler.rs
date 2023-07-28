bd119a10f02da108c8300067c2efc0fec813bddb
pbzweihander <pbzweihander@gmail.com>
2023-07-28 20:35:17 +0900

    Fallback to index.html

--- backend/src/handler.rs
+++ backend/src/handler.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
-use axum::Router;
+use axum::{http::Request, middleware::Next, response::Response, Router};
 use sea_orm::DatabaseConnection;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 
 use crate::config::CONFIG;
 
@@ -12,6 +12,17 @@ mod api;
 struct AppState {
     http_client: reqwest::Client,
     db: Arc<DatabaseConnection>,
+}
+
+async fn server_header_middleware<B>(req: Request<B>, next: Next<B>) -> Response {
+    let mut resp = next.run(req).await;
+    resp.headers_mut().insert(
+        "server",
+        format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+            .parse()
+            .unwrap(),
+    );
+    resp
 }
 
 pub fn create_router(db: DatabaseConnection) -> Router {
@@ -25,5 +36,11 @@ pub fn create_router(db: DatabaseConnection) -> Router {
     Router::new()
         .nest("/api", api)
         .with_state(state)
-        .nest_service("/", ServeDir::new(&CONFIG.static_files_directory_path))
+        .nest_service(
+            "/",
+            ServeDir::new(&CONFIG.static_files_directory_path).fallback(ServeFile::new(
+                CONFIG.static_files_directory_path.join("index.html"),
+            )),
+        )
+        .layer(axum::middleware::from_fn(server_header_middleware))
 }
